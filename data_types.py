5c8978e6dc1da94448bd80a5ac7e932a1b428de8
AntonV0 <anton.vlasenko94@gmail.com>
2026-01-27 10:10:00 +0000

    Add data_types.py

---
+++ data_types.py
@@ -0,0 +1,23 @@
+"""Core Data Types"""
+
+# Integer (int)
+x = 5
+print(type(x))
+
+# Floating point numbers (float)
+y = 3.14
+print(type(y))
+
+# String (str)
+z = "Hello, World!"
+print(type(z))
+
+# Boolean (bool)
+a = True
+print(type(a))
+
+# Reassign the variable data type and point to it
+y = 'new data'
+print(type(y))
+y = 8.9
+print(type(y))
